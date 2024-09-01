29663ecf7bdc846a8145d540a701efc7bcd1a516
hassanahmad9024 <marketingwork258@gmail.com>
2024-09-01 09:57:05 +0500

    making a water online project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,27 @@ import React from "react"
 
 const page = () => {
  return(
-<div>Hassan Ahmad</div>
+<div>
+    
+<figure className="bg-slate-100 rounded-xl dark:bg-slate-800">
+  <img className="w-24 h-32" src="/RYK Station.jpg" alt="" width="512" height="1080"></img>
+  <div className="pt-8 space-y-6">
+    <blockquote>
+      <p className="text-lg">
+        “Hi! I am Hassan Ahmad from Tranda Saway Khan.I am a student of Ai.”
+      </p>
+    </blockquote>
+    <figcaption>
+      <div>
+        Hassan Ahmad
+      </div>
+      <div>
+        Ai Student, RYK
+      </div>
+    </figcaption>
+  </div>
+</figure>
+</div>
  )
 }
 
